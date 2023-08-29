31c1b9849fec2925eca542052872692a03927ceb
kapi00z <hnerno.o@gmail.com>
2023-08-29 16:02:48 +0000

    autocommit

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+array=( $( ls . ) )
+base=("docker.sh" "python.sh" "yay.sh")
+
+for i in ${base[@]}
+do
+    if [[ ! " ${array[@]} " =~ " $i " ]]
+    then
+        echo "$i is not in directory!"
+        exit 1
+    fi
+done
+
+for i in ${base[@]}
+do
+    if [[ " ${array[@]} " =~ " $i " ]]
+    then
+        array=("${array[@]/$i}")
+    fi
+done
+
+#echo "${array[@]}"
+
+bash yay.sh
+bash docker.sh
+bash python.sh
+
+for i in ${array[@]}
+do
+    bash $i
+    echo $i
+done
